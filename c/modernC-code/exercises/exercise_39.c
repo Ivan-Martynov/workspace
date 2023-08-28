52fcd4720fd062fac3e77aa500687e979b0bc6e3
Ivan Martynov <ivan.a.martynov@gmail.com>
2023-08-28 16:02:49 +0300

    changes

---
+++ c/modernC-code/exercises/exercise_39.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void fgoto2(const unsigned int n)
+{
+    unsigned int j = 0;
+    unsigned int* p = 0;
+    unsigned int* q;
+
+AGAIN:
+    if (p)
+    {
+        printf("%u: p and q are %s, *p is %u\n", j,
+            (j > 1) ? "equal" : "unequal", *p);
+    }
+
+    q = p;
+    p = &j;
+
+    if (++j <= n)
+    {
+        goto AGAIN;
+    }
+}
+
+void fgoto(unsigned int n)
+{
+    unsigned int j = 0;
+    unsigned int* p = 0;
+    unsigned int* q;
+
+AGAIN:
+    if (p)
+    {
+        printf("%u: p and q are %s, *p is %u\n", j,
+            (j > 1) ? "equal" : "unequal", *p);
+    }
+
+    q = p;
+    p = &j;
+
+    if (++j <= n)
+    {
+        goto AGAIN;
+    }
+}
+
+int main(void)
+{
+    fgoto(2);
+
+    return EXIT_SUCCESS;
+}
