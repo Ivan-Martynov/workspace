c3c29a5a940b47642b9dff1d8984867caaaed96d
Ivan Martynov <ivan.a.martynov@gmail.com>
2024-03-19 15:56:32 +0200

    changes

---
+++ c/modernC-code/exercises/generic_examples.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <complex.h>
+#include <math.h>
+
+//#if defined(__STDC_IEC_559_COMPLEX__) || defined(__STDC_IEC_60559_COMPLEX__)
+#if 1
+#define abs(X)                                                                 \
+    _Generic((X),\
+    float: fabsf,\
+    long double: fabsl,\
+    float complex: cabsf,\
+    double complex: cabs,\
+    long double complex: cabsl,\
+    default: fabs)(X)
+#else
+#define abs(X)                                                                \
+    _Generic((X), float : fabsf, long double : fabsl, default : fabs)(X)
+#endif
+
+static inline double min(const double a, const double b)
+{
+    return a < b ? a : b;
+}
+
+static inline long double minl(const long double a, const long double b)
+{
+    return a < b ? a : b;
+}
+
+static inline float minf(const float a, const float b)
+{
+    return a < b ? a : b;
+}
+
+static inline size_t min_size_t(const size_t a, const size_t b)
+{
+    printf("Calling min_size_t\n");
+    return a < b ? a : b;
+}
+
+#define MIN_PTR_IMPL(PTR_1, PTR_2) ((PTR_1) < (PTR_2) ? (PTR_1) : (PTR_2) )
+#define MIN_PTR(A, B) MIN_PTR_IMPL((void*) {0} = (A), (void*) {0} = (B))
+
+static inline void* minp(void* a_ptr, void* b_ptr)
+{
+    return MIN_PTR(a_ptr, b_ptr);
+}
+
+#define min(A, B)       \
+    _Generic((B) - (A), \
+    float: minf,        \
+    double: min,        \
+    long double: minl,  \
+    size_t: min_size_t, \
+    default: minp) ((A), (B))
+
+int main()
+{
+    double complex z = 1.0 + 1.0*I;
+    printf("%.1f%+.1fi cartesian is rho=%f theta=%f polar\n",
+           creal(z), cimag(z), abs(z), carg(z));
+    
+    float a_f = 2.3f;
+    float b_f = -5.2f;
+    printf("Min of (%f, %f) = %f\n", a_f, b_f, min(a_f, b_f));
+    printf("Min of pointers (%p, %p) = %p\n", (void*)&a_f, (void*)&b_f,
+        min(&a_f, &b_f));
+    size_t a_zu = 3;
+    size_t b_zu = 5;
+    printf("Min of (%zu, %zu) = %zu\n", a_zu, b_zu, min(a_zu, b_zu));
+
+    return 0;
+}
--- c/modernC-code/exercises/macros_examples.c
+++ c/modernC-code/exercises/macros_examples.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
 #define MINSIZE(X, Y) (sizeof(X) < sizeof(Y) ? sizeof(X) : sizeof(Y))
 
@@ -100,13 +102,206 @@ static inline const char* string_literal(const char str[static 1])
         }                                                                      \
     } while (false)
 
+// Print multiple arguments in the same trace.
+#define TRACE_PRINT6(F, ...)                                                   \
+    do                                                                         \
+    {                                                                          \
+        if (TRACE_ON)                                                          \
+        {                                                                      \
+            fprintf(stderr, "%s:" STRGY(__LINE__) ": " F "\n", __func__,       \
+                __VA_ARGS__);                                                  \
+        }                                                                      \
+    } while (false)
+
+// Only trace with a text message - no values printed.
+#define TRACE_PRINT7(...)                                                      \
+    do                                                                         \
+    {                                                                          \
+        if (TRACE_ON)                                                          \
+        {                                                                      \
+            fprintf(stderr, "%s:" STRGY(__LINE__) ": " __VA_ARGS__ "\n",       \
+                __func__);                                                     \
+        }                                                                      \
+    } while (false)
+
+#define TRACE_FIRST_0(X, ...) X
+#define TRACE_FIRST(...) TRACE_FIRST_0(__VA_ARGS__, 0)
+
+#define TRACE_LAST_0(X, ...) __VA_ARGS__
+#define TRACE_LAST(...) TRACE_LAST_0(__VA_ARGS__, 0)
+
+// Only trace with a text message - no values printed.
+#define TRACE_PRINT8(...)                                                      \
+    TRACE_PRINT6(TRACE_FIRST(__VA_ARGS__) "%.0d", TRACE_LAST(__VA_ARGS__))
+
+// Trace with textual representation of the arguments.
+#define TRACE_PRINT9(F, ...) TRACE_PRINT6("(" #__VA_ARGS__ ") " F, __VA_ARGS__)
+
+// Call a three-parameter function with default arguments set to zero.
+#define ZERO_DEFAULT3_0(FUNC, X, Y, Z, ...) FUNC(X, Y, Z)
+#define ZERO_DEFAULT3(FUNC, ...) ZERO_DEFAULT3_0(FUNC, __VA_ARGS__, 0, 0, )
+
+#define strtoul(...) ZERO_DEFAULT3(strtoul, __VA_ARGS__)
+#define strtoull(...) ZERO_DEFAULT3(strtoull, __VA_ARGS__)
+#define strtol(...) ZERO_DEFAULT3(strtol, __VA_ARGS__)
+
+// Skip initial part of the argument.
+static inline const char* trace_skip(const char expr[static 1])
+{
+    for (const char* p = expr + 1; *p; ++p)
+    {
+        // End of head expects the 3 chars ``", ''
+        if (p[0] == '"')
+        {
+            return p + 3;
+        }
+
+        // Watch out for escaped " characters
+        if ((p[0] == '\\') && (p[1] == '"'))
+        {
+            ++p;
+        }
+    }
+
+    return 0;
+}
+
+/**
+ * @brief Function to print a list of values.
+ *
+ * @param stream
+ * @param func
+ * @param line
+ * @param expr
+ * @param head
+ * @param len
+ * @param arr
+ *
+ * @remark Only call this function through the macro ::TRACE_VALUES, which will
+ * provide the necessary contextual information.
+ *
+ * @version 0.1
+ *
+ * @date 2024-03-19
+ */
+static inline void trace_values(FILE* stream,
+    const char func[restrict static 1], const char line[restrict static 1],
+    const char expr[restrict static 1], const char head[restrict static 1],
+    const size_t len, const long double arr[static len])
+{
+    fprintf(stream, "%s:%s:(%s) %s %Lg", func, line, trace_skip(expr), head,
+        arr[0]);
+
+    for (size_t i = 1; i < len - 1; ++i)
+    {
+        fprintf(stream, ", %Lg", arr[i]);
+    }
+
+    fprintf(stream, "\n");
+}
+
+#define ALEN_0(_00, _01, _02, _03, _04, _05, _06, _07, _08, _09, _0A, _0B,     \
+    _0C, _0D, _0E, _0F, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _1A, \
+    _1B, _1C, _1D, _1E, _1F, ...)                                              \
+    _1F
+
+// Number of arguments in the ... list. This version works for lists with up to
+// 32 elements. An empty argument list is taken as one (empty argument)
+#define ALEN(...)                                                              \
+    ALEN_0(__VA_ARGS__, 0x1F, 0x1E, 0x1D, 0x1C, 0x1B, 0x1A, 0x19, 0x18, 0x17,  \
+        0x16, 0x15, 0x14, 0x13, 0x12, 0x11, 0x10, 0x0F, 0x0E, 0x0D, 0x0C,      \
+        0x0B, 0x0A, 0x09, 0x08, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01,      \
+        0x00)
+
+#define TRACE_VALUES_0(NARGS, EXPR, HEAD, ...)                                 \
+    do                                                                         \
+    {                                                                          \
+        if (TRACE_ON)                                                          \
+        {                                                                      \
+            if (NARGS > 1)                                                     \
+            {                                                                  \
+                trace_values(stderr, __func__, STRGY(__LINE__), "" EXPR "",    \
+                    "" HEAD "", NARGS,                                         \
+                    (long double const[NARGS]) {__VA_ARGS__});                 \
+            }                                                                  \
+            else                                                               \
+            {                                                                  \
+                fprintf(                                                       \
+                    stderr, "%s:" STRGY(__LINE__) ": %s\n", __func__, HEAD);   \
+            }                                                                  \
+        }                                                                      \
+    } while (false)
+
+#define TRACE_VALUES(...)                                                      \
+    TRACE_VALUES_0(ALEN(__VA_ARGS__), #__VA_ARGS__, __VA_ARGS__, 0)
+
+static inline double sum_array(const size_t n, const double arr[static n])
+{
+    double result = 0.0;
+
+    for (size_t i = 0; i < n; ++i)
+    {
+        result += arr[i];
+    }
+
+    return result;
+}
+#define SUM_VALUES(...)                                                        \
+    sum_array(                                                                 \
+        ALEN(__VA_ARGS__), (double const[ALEN(__VA_ARGS__)]) {__VA_ARGS__})
+
+FILE* g_io_debug = NULL;
+
+// Print to the debug stream @g_io_debug.
+#ifdef __GNUC__
+__attribute__((format(printf, 1, 2)))
+#endif
+
+static int print_debug(const char* const format, ...)
+{
+    int result = 0;
+
+    if (g_io_debug)
+    {
+        va_list args_list;
+        va_start(args_list, format);
+        result = vfprintf(g_io_debug, format, args_list);
+        va_end(args_list);
+    }
+
+    return result;
+}
+
+static double sum_arr_variadic(const size_t n, ...)
+{
+    double result = 0.0;
+
+    va_list args_list;
+    va_start(args_list, n);
+    for (size_t i = 0; i < n; ++i)
+    {
+        result += va_arg(args_list, double);
+    }
+    va_end(args_list);
+
+    return result;
+}
+
 static void test_values(void)
 {
     int x = 3;
     TRACE_VALUE0("Value", x);
     TRACE_PTR1("Pointer", &x);
 
-    TRACE_PRINT5("Checking integer: %d", x * 4);
+    // TRACE_PRINT6("Checking values: %d; %d; %g", x, x * 4, x + 3.2);
+    TRACE_PRINT8("Just a string.");
+    TRACE_PRINT8("Checking values: %d; %d; %g", x, x * 4, x + 3.2);
+    TRACE_PRINT9("Checking values: %d; %d; %g", x, x * 4, x + 3.2);
+
+    const double sum = SUM_VALUES(x, x * 4, x + 3.2);
+    TRACE_VALUES("", x, x * 4, x + 3.2, sum);
+
+    print_debug("Test %g\n", sum_arr_variadic(4, 12.2, 3.9, -23.898, 12043));
 }
 
 static void test_string(const char* const text)
@@ -119,6 +314,10 @@ static void test_string(const char* const text)
     if (text)
     {
         printf("String = %s\n", string_literal("Test string."));
+#if 1
+        const unsigned long value = strtoul(text);
+        printf("Trying to convert text \"%s\" to number => %lu\n", text, value);
+#endif
     }
     else
     {
