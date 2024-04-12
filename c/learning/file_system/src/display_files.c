e2618e2bb602a9013bfd3ad4d1f5a9c892a5598e
Ivan Martynov <ivan.a.martynov@gmail.com>
2024-04-12 15:54:48 +0300

    changes

--- c/learning/file_system/src/display_files.c
+++ c/learning/file_system/src/display_files.c
@@ -6,28 +6,106 @@
 #include <string.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <errno.h>
 #include "mrvn_string_helper.h"
 
-static bool not_rel_path_wide(const wchar_t* const path)
+#ifndef __STDC_ISO_10646__
+#error " wchar_t   wide   characters   have  to be  Unicode   code   points "
+#endif
+#ifdef __STDC_MB_MIGHT_NEQ_WC__
+#error " basic   character   codes   must   agree  on  char   and  wchar_t "
+#endif
+
+static size_t multibyte_string_length(const char src[1])
 {
-    return (wcscmp(path, L".") != 0) && (wcscmp(path, L"..") != 0);
+    mbstate_t state;
+    memset(&state, 0, sizeof(state));
+
+    size_t len = mbsrtowcs(NULL, &src, 0, &state);
+
+    // Restore errno on error.
+    if (len == (size_t)-1)
+    {
+        errno = 0;
+    }
+
+    return len;
+}
+
+static void test_size_multibyte_string(const char src[static 1])
+{
+    const size_t n = strlen(src);
+    size_t len = multibyte_string_length(src);
+
+    printf("Size of \"%s\": %zu, %zu\n", src, n, len);
+
+    for (size_t i = 0; i < n; ++i)
+    {
+        const unsigned char c = src[i];
+        printf("%c(", c);
+
+        if (c & 0x80)
+        {
+            printf("M");
+        }
+
+        printf("b 0x%04X),", (unsigned int)c);
+    }
+    printf("]\n");
+}
+
+static bool not_rel_path(const char path[static 1])
+{
+    return (strcmp(path, ".") != 0) && (strcmp(path, "..") != 0);
 }
 
 static int dir_selector(const struct dirent* dir)
 {
-    return not_rel_path_wide(dir->d_name);
+    return not_rel_path(dir->d_name);
 }
 
-static void show_files_wide(const wchar_t* const path_wide)
+static void show_files(const char path[static 1])
 {
     struct dirent** name_list;
+#if 0
     const int scan_result
-        = scandir(path_wide, &name_list, dir_selector, alphasort);
+        = scandir(path, &name_list, dir_selector, alphasort);
+#else
+    const int scan_result
+        = scandir(path, &name_list, dir_selector, alphasort);
+#endif
+    if (scan_result >= 0)
+    {
+        printf("Got %d items in %s\n", scan_result, path);
+        for (int i = 0; i < scan_result; ++i)
+        {
+            struct dirent* dir = name_list[i];
+
+            char full_path[4096] = "\0";
+            strcpy(full_path, path);
+
+            if ((path[strlen(path) - 1] != '/')
+                && (path[strlen(path) - 1] != '\\'))
+            {
+                strcat(full_path, "/");
+            }
+
+            strcat(full_path, dir->d_name);
+
+            test_size_multibyte_string(dir->d_name);
+
+            if (dir->d_type == DT_DIR)
+            {
+                show_files(full_path);
+            }
+        }
+    }
 }
 
 static void process_file_path_as_wide(const char* const src)
 {
-    if (!mrvn_null_or_empty(src))
+#if 1
+    if (mrvn_null_or_empty(src))
     {
         return;
     }
@@ -40,8 +118,11 @@ static void process_file_path_as_wide(const char* const src)
     }
 
     mbstowcs(str_wide, src, n);
+#endif
 
-    show_files_wide(str_wide);
+    printf("Show files\n");
+    show_files(src);
+    // show_files(str_wide);
 
     free(str_wide);
 }
--- cpp/file_system/src/example2.cpp
+++ cpp/file_system/src/example2.cpp
@@ -1,17 +1,90 @@
+#include <cstring>
 #include <filesystem>
 #include <iostream>
-#include <cstring>
 #include <string>
+
+union mbstate_t_ {
+  unsigned char bytes[sizeof(mbstate_t)];
+  mbstate_t state;
+} mbstate_t_;
+
+static size_t multibyte_string_length(const char src[1])
+{
+    std::mbstate_t state {};
+    std::size_t len {std::mbsrtowcs(nullptr, &src, 0, &state)};
+
+    // Restore errno on error.
+    if (len == static_cast<std::size_t>(-1))
+    {
+        errno = 0;
+    }
+
+    return len;
+}
+
+static size_t multibyte_string_length(
+    const std::string& src, const mbstate_t* state_ptr)
+{
+    if (!state_ptr)
+    {
+        return multibyte_string_length(src.data());
+        //union mbstate_t_ t {};
+        //state_ptr = &t.state;
+    }
+
+    mbstate_t state = *state_ptr;
+    const char* src_data {src.data()};
+    size_t len = std::mbsrtowcs(nullptr, &src_data, 0, &state);
+
+    if (len == static_cast<size_t>(-1))
+    {
+        errno = 0;
+    }
+
+    return len;
+}
+
+static void test_size_multibyte_string(const std::string& src)
+{
+    const size_t n {src.size()};
+#if 0
+    const size_t len {std::mbstowcs(nullptr, src.c_str(), n)};
+#else
+    size_t len {multibyte_string_length(src, nullptr)};
+#endif
+
+    std::cout << "Size of " << std::quoted(src) << ": " << n << ", " << len
+              << '\n';
+    std::cout << std::hex << std::uppercase << std::setfill('0') << "[";
+    for (const unsigned char c : src)
+    {
+        std::cout << c;
+        if (c & 0x80)
+        {
+            std::cout << "(Mb ";
+        }
+        else
+        {
+            std::cout << "(B ";
+        }
+        std::cout << "0x" << std::setw(2) << static_cast<int>(c) << "),";
+    }
+    std::cout << std::dec << "]\n";
+}
 
 static void display_file_names(const std::filesystem::path& dir_path,
     const bool recursive = false, size_t depth = 0)
 {
+#if 0
     for (size_t i {0}; i < depth; ++i)
     {
         std::cout << " ";
     }
     std::cout << "┌" << dir_path << "\n";
     ++depth;
+#else
+    test_size_multibyte_string(dir_path.string());
+#endif
 
     for (const std::filesystem::directory_entry& item :
         std::filesystem::directory_iterator {dir_path})
@@ -25,11 +98,15 @@ static void display_file_names(const std::filesystem::path& dir_path,
         }
         else
         {
+#if 0
             for (size_t i {0}; i < depth; ++i)
             {
                 std::cout << " ";
             }
             std::cout << "├" << item.path() << "\n";
+#else
+            test_size_multibyte_string(item.path().string());
+#endif
         }
     }
 }
@@ -47,28 +124,6 @@ static void process_directory(const std::string& directory_path)
     }
 }
 
-static void test_size_multibyte_string(const std::string& src)
-{
-    const size_t n {src.size()};
-    const size_t len {std::mbstowcs(nullptr, src.c_str(), n)};
-    std::cout << "Size of src: " << src << ": " << n << ", " << len << ", "
-              << src.length() << '\n';
-    std::cout << std::hex << std::uppercase;
-    for (const unsigned char c : src)
-    {
-        if (c & 0x80)
-        {
-            std::cout << "Multibyte => ";
-        }
-        else
-        {
-            std::cout << "Byte => ";
-        }
-        std::cout << c << '(' << static_cast<int>(c) << ") ";
-    }
-    std::cout << std::dec << '\n';
-}
-
 int main(const int argc, const char* argv[])
 {
     std::setlocale(LC_ALL, "");
@@ -76,7 +131,7 @@ int main(const int argc, const char* argv[])
     std::string item_path {argc > 1 ? argv[1] : "."};
 
     process_directory(item_path);
-    test_size_multibyte_string(item_path);
+    //test_size_multibyte_string(item_path);
 
     return 0;
 }
