4b1aad856b16a1dd82649f4a7e5da2b5bfd373ca
Ivan-Martynov <ivan.a.martynov@gmail.com>
2024-04-12 21:07:53 +0300

    changes

--- cpp/file_system/src/example2.cpp
+++ cpp/file_system/src/example2.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <iostream>
 #include <string>
+#include <fstream>
 
 union mbstate_t_ {
   unsigned char bytes[sizeof(mbstate_t)];
@@ -53,8 +54,9 @@ static void test_size_multibyte_string(const std::string& src)
     size_t len {multibyte_string_length(src, nullptr)};
 #endif
 
-    std::cout << "Size of " << std::quoted(src) << ": " << n << ", " << len
+    std::cout << std::quoted(src) << ": size = " << n << ", length = " << len
               << '\n';
+#if 0
     std::cout << std::hex << std::uppercase << std::setfill('0') << "[";
     for (const unsigned char c : src)
     {
@@ -70,6 +72,49 @@ static void test_size_multibyte_string(const std::string& src)
         std::cout << "0x" << std::setw(2) << static_cast<int>(c) << "),";
     }
     std::cout << std::dec << "]\n";
+#endif
+}
+
+static std::string wstring_to_string(
+    const std::wstring& str, std::mbstate_t* state_ptr)
+{
+    const wchar_t* src {str.c_str()};
+    const std::size_t n = std::wcsrtombs(nullptr, &src, 0, state_ptr) + 1;
+
+    std::string line(n, '\0');
+    std::wcsrtombs(line.data(), &src, n, state_ptr);
+
+    return line;
+}
+
+static std::string wstring_to_string(const std::wstring& str)
+{
+    std::mbstate_t state {};
+    return wstring_to_string(str, &state);
+}
+
+static void read_txt_file(const std::filesystem::path& file_path)
+{
+    if (file_path.extension() != ".txt")
+    {
+        return;
+    }
+
+    std::wifstream stream {file_path, std::ios::in};
+    stream.imbue(std::locale(""));
+
+    while (true)
+    {
+        std::wstring str {};
+        stream >> str;
+        if (str.empty())
+        {
+            break;
+        }
+
+        std::string line {wstring_to_string(str)};
+        std::cout << "Got line: " << line << "; size = " << line.size() << "\n";
+    }
 }
 
 static void display_file_names(const std::filesystem::path& dir_path,
@@ -83,7 +128,7 @@ static void display_file_names(const std::filesystem::path& dir_path,
     std::cout << "┌" << dir_path << "\n";
     ++depth;
 #else
-    test_size_multibyte_string(dir_path.string());
+    test_size_multibyte_string(dir_path.filename());
 #endif
 
     for (const std::filesystem::directory_entry& item :
@@ -105,7 +150,8 @@ static void display_file_names(const std::filesystem::path& dir_path,
             }
             std::cout << "├" << item.path() << "\n";
 #else
-            test_size_multibyte_string(item.path().string());
+            test_size_multibyte_string(item.path().filename());
+            read_txt_file(item.path());
 #endif
         }
     }
@@ -131,7 +177,8 @@ int main(const int argc, const char* argv[])
     std::string item_path {argc > 1 ? argv[1] : "."};
 
     process_directory(item_path);
-    //test_size_multibyte_string(item_path);
+
+    //std::cout << "User locale: " << std::locale("").name() << "\n";
 
     return 0;
 }
