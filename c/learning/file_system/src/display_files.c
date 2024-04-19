45efcbfdc68dba244728fa8d350d0479be9ed96f
Ivan Martynov <ivan.a.martynov@gmail.com>
2024-04-19 15:59:08 +0300

    changes

--- c/learning/file_system/src/display_files.c
+++ c/learning/file_system/src/display_files.c
@@ -93,7 +93,7 @@ static int wrename_path(const wchar_t old_name_wide[static 1],
 static bool check_mp3_file_header(const char file_path[static 1])
 {
     const char* const dot_place = strrchr(file_path, '.');
-    if (!dot_place || (mrvn_compare_case_insensitive(dot_place + 1, "mp3") != 0))
+    if (!dot_place || (mrvn_compare_case_insensitive(dot_place, ".mp3") != 0))
     {
         // fprintf(stderr, "File %s doesn't have mp3 extension.\n", file_path);
         return false;
@@ -142,10 +142,11 @@ static bool check_mp3_file_header(const char file_path[static 1])
     return true;
 }
 
-static size_t get_wide_char_line(
-    wchar_t** target_ptr, size_t* count_ptr, FILE* file_stream)
+#if 1
+static size_t allocate_wchar_line_from_file_stream(
+    wchar_t** pp_target, size_t* count_ptr, FILE* file_stream)
 {
-    if (!target_ptr || !count_ptr)
+    if (!pp_target || !count_ptr)
     {
         errno = EINVAL;
         return -1;
@@ -157,39 +158,93 @@ static size_t get_wide_char_line(
     }
 
     size_t result = 0;
-    const size_t chunk_size = 256;
+    const size_t chunk_size = 16;
     wchar_t line[chunk_size];
-    while (fgetws(line, chunk_size, file_stream))
+#if 1
+    while (true)
     {
-        wchar_t* ptr = wcschr(line, L'\n');
-        if (ptr)
+        size_t i = 0;
+        bool found_end = false;
+        while (i < chunk_size)
         {
-            *ptr = L'\0';
-            ptr = wcschr(line, L'\r');
-            if (ptr)
+            const wint_t wide_char = fgetwc(file_stream);
+            if ((wide_char == WEOF)
+                || ((wide_char == L'\r') && (fgetwc(file_stream) == L'\n'))
+                || (wide_char == L'\n'))
             {
-                *ptr = L'\0';
+                found_end = true;
+                //++i;
+                break;
             }
+
+            line[i++] = wide_char;
         }
+        line[i++] = L'\0';
 
-        const size_t len = wcslen(line);
-        const size_t target_len = *target_ptr ? wcslen(*target_ptr) : 0;
-
-        ptr = realloc(
-            *target_ptr, (target_len + len + 1) * sizeof(**target_ptr));
+        const size_t target_len = *pp_target ? wcslen(*pp_target) : 0;
+        const size_t alloc_size = (target_len + i) * sizeof(**pp_target);
+        wchar_t* ptr = realloc(*pp_target, alloc_size);
         if (!ptr)
         {
             return -1;
         }
+        *pp_target = ptr;
+        memcpy(*pp_target + target_len, line, i * sizeof(**pp_target));
+        *count_ptr += i + 1;
+        result += alloc_size;
+
+        if (found_end)
+        {
+            break;
+        }
+    }
+    *pp_target[*count_ptr] = L'\0';
+    return result;
+#endif
+    while (fgetws(line, chunk_size, file_stream))
+    {
+        wchar_t* ptr = line;
+        for (; *ptr; ++ptr)
+        {
+            if ((*ptr == L'\r') || (*ptr == L'\n'))
+            {
+                *ptr = L'\0';
+                break;
+            }
+        }
+
+        const size_t len = wcslen(line) + 1;
+        const size_t target_len = *pp_target ? wcslen(*pp_target) : 0;
+
+        const size_t alloc_size = (target_len + len) * sizeof(**pp_target);
+        ptr = realloc(*pp_target, alloc_size);
+        if (!ptr)
+        {
+            return -1;
+        }
+        *pp_target = ptr;
+#if 1
+        memcpy(*pp_target + target_len, line, len * sizeof(**pp_target));
+#else
         wcscat(*target_ptr, line);
+#endif
         result += len;
     }
 
+    *count_ptr = result;
+
     return result;
 }
+#endif
 
 static void read_multibyte_text_file(const char file_path[static 1])
 {
+    const char* const dot_place = strrchr(file_path, '.');
+    if (!dot_place || (mrvn_compare_case_insensitive(dot_place, ".txt") != 0))
+    {
+        return;
+    }
+
     FILE* file_stream = fopen(file_path, "r");
     if (!file_stream)
     {
@@ -199,31 +254,98 @@ static void read_multibyte_text_file(const char file_path[static 1])
 
     size_t word_count = 0;
     size_t symbol_count = 0;
+    const bool verbose = true;
+    const size_t max_byte_per_wide_char = 32;
+    size_t byte_count[max_byte_per_wide_char];
+    for (size_t i = 0; i < max_byte_per_wide_char; ++i)
+    {
+        byte_count[i] = 0;
+    }
 
 #if defined __STDC_ALLOC_LIB__ && (__STDC_WANT_LIB_EXT2__ == 1)
     wchar_t* str = NULL;
-    size_t n = 0;
     while (getwline(&str, &n, file_stream) != -1)
+#else
+    //    while (allocate_wchar_line_from_file_stream(&str, &n, file_stream)
+    //           != (size_t)-1)
+    printf("\n%s:\n", file_path);
+
+    // const size_t chunk_size = 256;
+    // wchar_t str[chunk_size];
+    wchar_t* line_ptr = NULL;
+    size_t n = 0;
+    while (allocate_wchar_line_from_file_stream(&line_ptr, &n, file_stream)
+           != (size_t)-1)
+    // while (fgetws(str, chunk_size, file_stream))
     {
-        bool found_graphic_symbol = false;
-        for (size_t i = 0; i < n; ++i)
+        bool found_symbol = false;
+        for (const wchar_t* ptr = line_ptr; *ptr != L'\0'; ++ptr)
         {
-            if (iswgraph(str[i]))
+            // const wchar_t wide_char = str[i];
+            const wchar_t wide_char = *ptr;
+            if (iswgraph(wide_char))
             {
+                if (verbose)
+                {
+                    wctomb(NULL, wide_char);
+                    char symbol_string[MB_CUR_MAX];
+                    const int m = wctomb(symbol_string, wide_char);
+                    if (m > 0)
+                    {
+                        symbol_string[m] = '\0';
+                        printf("%s", symbol_string);
+                        if ((size_t)m < max_byte_per_wide_char)
+                        {
+                            ++byte_count[m];
+                        }
+                    }
+                }
+
                 ++symbol_count;
-                found_graphic_symbol = true;
-            }
-            else
-            {
-                if (found_graphic_symbol)
+                if (!found_symbol)
                 {
                     ++word_count;
                 }
-                found_graphic_symbol = false;
+                found_symbol = true;
+            }
+            else
+            {
+                if (verbose && found_symbol)
+                {
+                    printf(" ");
+                }
+                found_symbol = false;
             }
         }
     }
-    free(str);
+    free(line_ptr);
+    printf("\n\t%zu words and %zu symbols\n", word_count, symbol_count);
+    // printf("\nFound %zu words and %zu symbols at %s\n", word_count,
+    //    symbol_count, file_path);
+    for (size_t i = 0; i < max_byte_per_wide_char; ++i)
+    {
+        const size_t count = byte_count[i];
+        if (count > 0)
+        {
+            if (count > 1)
+            {
+                printf("\t%zu symbols, each occupying ", count);
+            }
+            else
+            {
+                printf("\t1 symbol occupying ");
+            }
+
+            if (i > 1)
+            {
+                printf("%zu bytes\n", i);
+            }
+            else
+            {
+                printf("1 byte\n");
+            }
+        }
+    }
 #endif
 }
 
