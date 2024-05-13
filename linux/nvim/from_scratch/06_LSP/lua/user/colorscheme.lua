58e33af2a1168b9c979202b6875b6136f8069709
Ivan-Martynov <ivan.a.martynov@gmail.com>
2024-05-13 08:04:38 +0300

    changes

---
+++ linux/nvim/from_scratch/06_LSP/lua/user/colorscheme.lua
@@ -0,0 +1,10 @@
+local colorscheme = "solarized"
+vim.cmd "set background=light"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+
+if not status_ok then
+    vim.notify("colorscheme " .. colorscheme .. " not found!")
+    return
+end
+
