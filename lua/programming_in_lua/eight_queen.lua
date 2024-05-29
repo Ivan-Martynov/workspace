13daf0bdbe44b102d620d803d9a44600f803ccb2
Ivan-Martynov <ivan.a.martynov@gmail.com>
2024-05-29 08:17:37 +0300

    changes

--- lua/programming_in_lua/eight_queen.lua
+++ lua/programming_in_lua/eight_queen.lua
@@ -1,9 +1,13 @@
 local N = 8
 
+local check_count = 0
+
 -- Check whether position (n, c) is safe.
 local function is_place_ok(a, n, c)
+    check_count = check_count + 1
     for i = 1, n - 1 do
-        if (a[i] == c) or (a[i] - i == c - n) or (a[i] + i == c + n) then
+        --if (a[i] == c) or (a[i] - i == c - n) or (a[i] + i == c + n) then
+        if (a[i] == c) or (math.abs(a[i] - c) == math.abs(n - i)) then
             return false
         end
     end
@@ -21,17 +25,27 @@ local function print_board(a)
     io.write("\n")
 end
 
+local solution_count = 0
 local function add_queen(a, n)
     if n > N then
+        solution_count = solution_count + 1
         print_board(a)
+        --return true
     else
         for c = 1, N do
             if is_place_ok(a, n, c) then
                 a[n] = c
                 add_queen(a, n + 1)
+                --if (add_queen(a, n + 1)) then
+                    --return true
+                --end
             end
         end
+        return false
     end
 end
 
 add_queen({}, 1)
+
+print("Check performed " .. check_count .. " times.")
+print("Solution count = " .. solution_count)
---
+++ lua/programming_in_lua/exercise_2_2.lua
@@ -0,0 +1,81 @@
+local N = tonumber(arg[1]) or 8
+
+local function perm_gen(a, n, res)
+    if n == 0 then
+        local row = {}
+        for _, v in ipairs(a) do
+            table.insert(row, v)
+        end
+        table.insert(res, row)
+    else
+        for i = 1, n do
+            a[n], a[i] = a[i], a[n]
+
+            perm_gen(a, n - 1, res)
+
+            a[n], a[i] = a[i], a[n]
+        end
+    end
+end
+
+local function make_permutations(n)
+    local a = {}
+    for i = 1, n do
+        a[i] = i
+    end
+
+    local result = {}
+    perm_gen(a, #a, result)
+    return result
+end
+
+local permutations = make_permutations(N)
+
+local function print_board(a)
+    for i = 1, N do
+        io.write("│")
+        for j = 1, N do
+            io.write(a[i] == j and " ♛ │" or "   │")
+        end
+        io.write("\n")
+
+        if i ~= N then
+            io.write("├───")
+            for _ = 1, N - 1 do
+                io.write("┼───")
+            end
+            io.write("┤\n")
+        end
+    end
+    io.write("└───")
+    for _ = 1, N - 1 do
+        io.write("┴───")
+    end
+    io.write("┘\n")
+
+
+    io.write("\n")
+end
+
+local function is_solution(v)
+    for i = 1, N do
+        for j = i + 1, N do
+            if (math.abs(v[j] - v[i]) == math.abs(i - j)) then
+                return false
+            end
+        end
+    end
+
+    return true
+end
+
+local solution_count = 0
+for _, v in ipairs(permutations) do
+    if is_solution(v) then
+        solution_count = solution_count + 1
+        print_board(v)
+    end
+end
+
+print("Permutation count = " .. #permutations)
+print("Solution count = " .. solution_count)
